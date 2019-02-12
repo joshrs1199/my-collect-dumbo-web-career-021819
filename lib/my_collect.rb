6e970818be6c71481a72b072a7d7fa08e3aab970
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-12 06:26:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   mod_collection = []
   while i < collection.length
-    mod_collection << yield (collection[i])
+    mod_collection << yield(collection[i])
     i += 1
   end
   mod_collection
